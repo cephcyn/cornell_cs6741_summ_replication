eee61939e9fa0f892b9fc014e8df864607db6ac2
cephcyn <jyz@cs.cornell.edu>
2022-03-22 04:05:42 -0400

    add decsum implementation, updated readme

--- README.md
+++ README.md
@@ -6,27 +6,64 @@ Borrows some structure from the paper's released codebase [here](https://github.
 
 ## Writeup
 
-TODO misc notes for now...
+### Introduction
+
+The paper introduces the new task of "decision-focused summarization": generating summaries of a text to present relevant information for a decision being made that takes that text into account.
+This is in contrast to most previous work on text summarization, which has sought to create general summaries of texts.
+An example that the authors give to contrast these two scenarios is with medical diagnoses based on text medical notes: while medical text may contain information about some patient's foot injuries, if the text is being used for pancreatic cancer risk assessment, a summary of the text that contains information about foot injuries is very unlikely to be helpful.
+
+The authors make several clear contributions:
+- They build a dataset for this task (predicting Yelp score ratings based on the text of a review)
+- They propose three goals of a decision-focused summarization (faithfulness, representativeness, and sentence diversity)
+- They describe a summarization algorithm `DecSum` that selects sentences from a larger set of reviews based on these goals as a way of generating a summary
+- They evaluate `DecSum` against three text-based summarization methods (random text selection, `BART`, and `PreSumm`) and two decision-model-based summarization methods (`IG`, `Attention`) using mean squared error between predictions made by a decision model on the original texts and the summaries as the reference metric.
+- They evaluate these same summarization methods against each other with crowdsourced human decisions, judging on classification accuracy.
+
+I focus on replicating parts of the first four points: building a dataset, implementing `DecSum`, implementing a subset of the other summarization methods they used, and comparing their mean squared error performance.
+
+### Method
+
+Dataset collection:
+- The authors don't include their full dataset in the repository, so I re-downloaded the most recent version of the Yelp dataset and re-did preprocessing on that dataset with a new randomly selected train/dev/test dataset split in the same ratio that they used.
+- The dataset that I had is 15% larger than theirs. (Their test set had size=3623, my test set had size=4172.)
 
 Finetuning Longformer:
-- Based on pretrained `longformer-base`, as that is what the decsum codebase uses.
-- Finetuning `longformer-base` took approximately 4 hours (including restarting when out-of-memory) on a GeForce RTS 3090
+- I finetuned on pretrained `longformer-base-4096` (NOT `longformer-large-4096`), as that is what the decsum codebase uses.
+  Oddly enough, their appendix description seems to contradict their public codebase.
+  They describe using a model with 102M parameters, which matches the size of `longformer-large-4096`.
+  The Longformer paper reports that `longformer-base-4096` only has 41M parameters.
+  However, if I estimate model size based on training time, their reported training time (over 3 hours) matches up with how much time I'd guess training `longformer-base-4096` takes, since I spent only slightly less time than that training a `longformer-base-4096` model with a higher-spec GPU (I used a RTX 3090, they reported using a RTX Titan).
+- I use the exact same training hyperparameters as the authors describe in their appendix, other than changing the number of workers and the batch size.
 
 Summarization:
-- All summaries are cut off after N tokens
-- (Random) Prioritizes sentences in a random order, terminates adding sentences upon encountering one that will exceed the N-token limit, and then re-orders all sentences so that they are sorted the same way as they were in the original text
-- (DecSum) Excluding the "faithfulness" component of the DecSum summary quality metric, as that was reported to take over 10 hours per run on the test set.
-- (DecSum) My implementation of DecSum doesn't follow their pseudocode / algorithm that they describe in "Algorithm 1" of the paper perfectly, as there seemed to be a typo in that algorithm
+- Their appendix mentions that all sentence-based summaries are limited to N=50 tokens (words).
+- (Random) Prioritizes sentences in a random order, terminates adding sentences upon encountering one that will exceed the N-token limit, and then re-orders all sentences so that they are sorted the same way as they were in the original text.
+- (DecSum) I exclude the "faithfulness" component of the DecSum summary quality metric, as that was reported to take over 10 hours per run on the test set.
+- (DecSum) My implementation of DecSum doesn't follow their pseudocode / algorithm that they describe in "Algorithm 1" of the paper perfectly, as there seemed to be a typo in that algorithm.
   - The "X <- X - x" line causes X to decrease in size, which doesn't match up with how the overall loss functions are defined.
-  - Therefore, I would add an additional "Xoriginal <- X" before the while loop and replace all instances of "X" in the loss function calls with "Xoriginal"
+  - I would add an additional "Xoriginal <- X" before the while loop and replace all instances of "X" in the loss function calls with "Xoriginal".
 - (DecSum) Token count is based on SpaCy model token counting, *not* Longformer model token counting.
   - The original paper is not clear about how token counts were done. The implementation uses SpaCy model token counting.
   - The difference between these: SpaCy tends to use clearer rules for tokenization, and generally counts distinct words. Longformer tokenizer seems to pick up on word prefixes, suffixes, or other modifiers more often.
 
+### Results
+
+TODO
+
+Runtimes:
+- Collecting the dataset and preprocessing took approximately 5 minutes?
+- Finetuning `longformer-base-4096` took slightly under 3 hours (excluding restarting when out-of-memory?) on a GeForce RTX 3090.
+  - However, when I was initially trying to run this finetuning on Google Colab, the estimated finetuning time was over 36 hours (and it kicked me off of the server after about 4 hours of finetuning time).
+- Generating random summaries for the test dataset took approximately 5 minutes?
+- Generating DecSum(0,0,1) summaries for the test dataset took approximately 15 minutes?
+- Generating DecSum(0,1,0) summaries for the test dataset took TODO minutes
+- Generating DecSum(0,1,1) summaries for the test dataset took TODO minutes
+
+### Appendix: Tutorials referenced when implementing all of this...
+
 Demo training scripts referenced:
 - https://github.com/jlealtru/website_tutorials/blob/main/notebooks/Longformer%20with%20IMDB.ipynb (main reference)
 - https://pytorch.org/tutorials/beginner/data_loading_tutorial.html (Dataset class implementation)
-- https://github.com/huggingface/transformers/issues/7198 (checkpoint resuming)
 - https://huggingface.co/docs/transformers/tasks/sequence_classification
 - https://github.com/huggingface/notebooks/blob/master/transformers_doc/training.ipynb
 
@@ -74,8 +111,8 @@ Run once each for each type of summary (`SUMMARY_TYPE`) being evaluated:
 python -m generate_summaries --summary_type SUMMARY_TYPE
 ```
 
-TODO text-only summarization, model-only summarization
+TODO impl text-only summarization, model-only summarization
 
 ### 5. Evaluate
 
-TODO
+TODO cleanup this code...
--- generate_summaries.py
+++ generate_summaries.py
@@ -1,7 +1,15 @@
 import argparse
 import random
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import Trainer
+import datasets
 import spacy
+import torch
+from sentence_transformers import SentenceTransformer
+from scipy.stats import wasserstein_distance
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import pandas as pd
 
 import utils
 import constants
@@ -52,7 +60,7 @@ def truncate_sentence_ordering(ordered_sentences, token_limit):
 #   1. cut up reviews into sentences
 #   2. randomly order them
 #   3. Add all of them (in the same order as original data) until summary
-#      would exceed [constants.SUMMARY_TOKEN_LIMIT] tokens (incl begin and end)
+#      would exceed [constants.SUMMARY_TOKEN_LIMIT] tokens
 # Takes in an entire dataset, returns an entire dataset
 def generate_summary_random(
     dataset, spacy_nlp, tokenizer, token_limit=constants.SUMMARY_TOKEN_LIMIT
@@ -71,6 +79,122 @@ def generate_summary_random(
         # assume "begin" and "end" tokens (from longformer) are always going to be added to any individual text
         result_dataset.append({
             "reviews": utils.truncate_sentence_ordering(shuffled_sentences, token_limit-2),
+            "scores": dataset[entry_i]["scores"],
+            "business": dataset[entry_i]["business"],
+            "avg_score": dataset[entry_i]["avg_score"],
+        })
+    return result_dataset
+
+# Generate a DecSum summary: 
+#   1. cut up reviews into sentences
+#   2. select the {sentence_count_limit} number of sentences that minimizes 
+#      objective function via greedy beamsearch algorithm
+#   3. Add all of them (in the same order as original data) until summary
+#      would exceed [constants.SUMMARY_TOKEN_LIMIT] tokens
+# Takes in an entire dataset, returns an entire dataset
+# Note: if gamma_distinct!=0, then sentbert is required
+def generate_summary_decsum(
+    dataset, model, spacy_nlp, tokenizer, token_limit=constants.SUMMARY_TOKEN_LIMIT, 
+    sentence_count_limit=constants.SUMMARY_SENTENCE_LIMIT, 
+    beam_width=constants.DECSUM_BEAM_WIDTH,
+    alpha_faithful=0, beta_represent=1, gamma_distinct=1,
+    sentbert=None,
+):
+    # Do argument checks
+    if (gamma_distinct!=0) and (sentbert is None):
+        raise ValueError("sentbert is None, should be passed when gamma_distinct!=0")
+
+    # define faithfulness metric
+    def objective_f(selected, all_sentences, model):
+        if alpha_faithful==0:
+            return 0
+        raise NotImplementedError()
+    # define representativeness metric
+    def objective_r(selected, full_sentence_preds):
+        if beta_represent==0:
+            return 0
+        selected_preds = [full_sentence_preds[e[0]] for e in selected]
+        return np.log(wasserstein_distance(selected_preds, full_sentence_preds))
+    # define textual non-redundancy metric
+    def objective_d(selected, cos_similarity):
+        if gamma_distinct==0:
+            return 0
+        selected_ids = [e[0] for e in selected]
+        selected_cos_similarity = cos_similarity[selected_ids][:,selected_ids]
+        # Pick out the maximally different sentence per sentence, then sum
+        # Note that the diagonals were set to 0, so it'll avoid calculating self-similarity
+        return sum(np.apply_along_axis(max, 1, selected_cos_similarity))
+    
+    # Now summarize the dataset
+    result_dataset = []
+    for entry_i in range(len(dataset)):
+        # grab sorting and token information
+        sentences = utils.split_sentences(" ".join(dataset[entry_i]["reviews"]), spacy_nlp, tokenizer)
+        
+        # calculate prediction distributions per-sentence if we need to
+        full_sentence_preds = None
+        if beta_represent!=0:
+            # setup the dataset format
+            config_dataset = datasets.Dataset.from_dict({
+                "text": [e[1] for e in sentences],
+                "label": [e[0] for e in sentences],
+            }).map(lambda x: tokenizer(x["text"], padding="max_length", truncation=True, max_length=constants.MAX_SEQ_LENGTH))
+            config_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
+            # setup the trainer...
+            trainer = Trainer(
+                model=model,
+            )
+            # run predictions per-sentence
+            full_sentence_preds = trainer.predict(test_dataset=config_dataset)[0]
+            full_sentence_preds = np.reshape(full_sentence_preds, (len(full_sentence_preds),))
+
+        # Apply initial SentBERT pass if we need to
+        full_cos_similarity = None
+        if gamma_distinct!=0:
+            full_cos_similarity = cosine_similarity(sentbert.encode(sentences, convert_to_numpy=True))
+            # We're always going to be ignoring sentence self-similarity
+            np.fill_diagonal(full_cos_similarity, 0)
+        
+        # select the sentences that will be included with beam search!
+        # each beam candidate is in format [objective_score, selected_sentences, available_sentences]
+        beam_candidates = [[0, [], sentences]]
+        # Should we activate "objective_r only in first step"?
+        is_first_step = beta_represent!=0
+        while (
+            (len(beam_candidates[0][1])<sentence_count_limit) and    # we haven't hit the sentence count threshold yet
+            (len(beam_candidates[0][2])!=0) and                      # there's still sentences available to add
+            (sum([e[2] for e in beam_candidates[0][1]])<token_limit) # we haven't hit the token count threshold yet
+        ): 
+            # take out the next best option
+            current_candidate = beam_candidates[0]
+            beam_candidates = beam_candidates[1:]
+            # add all potential options
+            new_options = []
+            for s_i in range(len(current_candidate[2])):
+                considering_sentence = current_candidate[2][s_i]
+                considering_selected = current_candidate[1]+[considering_sentence]
+                considering_remnants = current_candidate[2][:s_i]+current_candidate[2][s_i+1:]
+                # in keeping with pseudocode, ONLY consider the representativeness metric on first step
+                new_options.append([
+                    (
+                        (not is_first_step)*alpha_faithful*objective_f(considering_selected, sentences, model)
+                        + beta_represent*objective_r(considering_selected, full_sentence_preds)
+                        + (not is_first_step)*gamma_distinct*objective_d(considering_selected, full_cos_similarity)
+                    ),
+                    considering_selected,
+                    considering_remnants
+                ])
+                is_first_step = False
+            beam_candidates = beam_candidates + new_options
+            # resort the beam candidates
+            beam_candidates.sort(key=lambda x:x[0])
+            # trim down the beam candidates
+            beam_candidates = beam_candidates[:min(4, len(beam_candidates))]
+        # we've outrunned the maximum sentence count / ran out of sentences we can add
+        selected_sentences = beam_candidates[0][1]
+
+        result_dataset.append({
+            "reviews": utils.truncate_sentence_ordering(selected_sentences, token_limit),
             "scores": dataset[entry_i]["scores"],
             "business": dataset[entry_i]["business"],
             "avg_score": dataset[entry_i]["avg_score"],
@@ -120,7 +244,34 @@ if __name__ == "__main__":
         utils.dump_jsonl_gz(summaries_random, random_outfile)
     elif args.summary_type[:6]=="decsum":
         logging.info(f"GENERATING DECSUM SUMMARIES")
-        pass
+        logging.info(f"need to initialize model for running...")
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        logging.info(f"will be running model; using device={device}")
+        sentbert = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens', device=device)
+        logging.info(f"loaded SentenceTransformer")
+        model_save_location = os.path.join(
+            constants.CHECKPOINT_DIR,
+            constants.LONGFORMER_SAVE
+        )
+        model = AutoModelForSequenceClassification.from_pretrained(
+            model_save_location, 
+            num_labels=1,
+        )
+        logging.info(f"loaded last checkpoint of finetuned longformer from {model_save_location}")
+        opt_f = int(args.summary_type[6])
+        opt_r = int(args.summary_type[7])
+        opt_d = int(args.summary_type[8])
+        summaries_decsum = generate_summary_decsum(
+            testset, model, nlp, tokenizer, 
+            beam_width=constants.DECSUM_BEAM_WIDTH,
+            alpha_faithful=opt_f, beta_represent=opt_r, gamma_distinct=opt_d,
+            sentbert=sentbert,
+        )
+        decsum_outfile = os.path.join(
+            constants.SUMMARY_DIR,
+            f"{constants.NUM_REVIEWS}reviews",
+            f"t{constants.SUMMARY_TOKEN_LIMIT}_decsum_{opt_f}{opt_r}{opt_d}_beam{constants.DECSUM_BEAM_WIDTH}.jsonl.gz",
+        )
         logging.info(f"DONE GENERATING DECSUM SUMMARIES, NOW SAVING TO {decsum_outfile}")
-        pass
+        utils.dump_jsonl_gz(summaries_decsum, decsum_outfile)
     logging.info(f"Done!")
